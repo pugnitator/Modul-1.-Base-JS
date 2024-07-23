d728836dbc4d05a27323257bdf33da49419a15a9
pugnitator <pugnitator2000@gmail.com>
2024-07-23 18:32:21 +0300

    M1_2_task2_Variables(About me)

---
+++ index.js
@@ -0,0 +1,12 @@
+let myName = 'Катя';
+let mySurname = 'Цверава';
+let myFavoriteDrink = 'чаёчек';
+let myFavoriteAnimal = 'котопёс';
+let myFavoriteProgrammingLanguage = 'kotlin';
+//JS ещё не успела полюбить, не бейте :D
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
