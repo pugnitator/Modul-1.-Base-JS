79aa499c36f9a4735537d23629cd99f38d083603
pugnitator <pugnitator2000@gmail.com>
2024-07-23 19:41:20 +0300

    Change maxCarSpeed

--- car.js
+++ car.js
@@ -5,3 +5,12 @@ let carOwner = 'Доминик';
 alert(carName);
 alert(maxCarSpeed);
 alert(carOwner);
+
+maxCarSpeed = 150;
+console.log(maxCarSpeed);
+
+maxCarSpeed = 280;
+console.log(maxCarSpeed);
+
+maxCarSpeed = 0;
+console.log(maxCarSpeed);
