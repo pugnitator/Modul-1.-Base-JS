fab083302e45911b8548ad180f100b88c02e977e
pugnitator <pugnitator2000@gmail.com>
2024-07-27 15:29:32 +0300

    M1_5_task2

--- M1_5_String/task1/index.js
+++ M1_5_String/task1/index.js
@@ -1,3 +1,4 @@
+//Task 1
 const myName = 'Ekaterina';
 const programmingLanguage = 'JavaScript';
 const courseCreatorName = 'Владилен Минин';
@@ -11,3 +12,16 @@ console.log(
 )
 
 console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучала ${programmingLanguage} ${numberOfMonth} месяц. Я уверена, что пройду данный курс до конца!`);
+
+
+//Task 2
+let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучала ${programmingLanguage} ${numberOfMonth} месяц. Я уверена, что пройду данный курс до конца!`;
+
+myInfoText = myInfoText
+.replaceAll(programmingLanguage, programmingLanguage.toLowerCase())
+.replaceAll('курс', 'КУРС');
+
+console.log(myInfoText);
+
+const stringLength = myInfoText.length;
+console.log(stringLength, myInfoText[0], myInfoText[stringLength - 1]);
