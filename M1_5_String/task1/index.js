b23729c71b61e1ba8e09be6338629b85f0019bf7
pugnitator <pugnitator2000@gmail.com>
2024-07-27 15:04:04 +0300

    M1_5_task1. Concatination

---
+++ M1_5_String/task1/index.js
@@ -0,0 +1,13 @@
+const myName = 'Ekaterina';
+const programmingLanguage = 'JavaScript';
+const courseCreatorName = 'Владилен Минин';
+const reasonText = 'это интересно';
+const numberOfMonth = 1;
+
+console.log(
+    'Всем привет! Меня зовут ' + myName + '. Сейчас я изучаю язык программирования ' +
+     programmingLanguage + ' на курсе по ' + programmingLanguage + ' у ' + courseCreatorName + '. Я хочу стать веб-разработчиком, потому что '
+     + reasonText + '. До этого я изучала ' + programmingLanguage + ' ' + numberOfMonth + ' месяц. Я уверена, что пройду данный курс до конца!'
+)
+
+console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучала ${programmingLanguage} ${numberOfMonth} месяц. Я уверена, что пройду данный курс до конца!`);
