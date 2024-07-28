283ae3917da09af8e46c67cd7a39e639a4dfb62c
pugnitator <pugnitator2000@gmail.com>
2024-07-28 15:09:15 +0300

    save changes

---
+++ confirm/index.js
@@ -0,0 +1,6 @@
+const answer1 = confirm('JavaScript появился в 1995 году?');
+if(answer1 === true) alert('Ответ верный')
+else alert('Правильный ответ: Да')
+
+const answer2 = confirm('Спецификация JavaScript называется ECMAScript?');
+const answer3 = confirm('JavaScript был создан за 1 месяц?');
