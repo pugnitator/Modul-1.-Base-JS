27b93c178b0c4515669dac01c7d57447a8c9fad8
pugnitator <pugnitator2000@gmail.com>
2024-07-28 17:25:03 +0300

    M1_6_task3

--- confirm/index.js
+++ confirm/index.js
@@ -1,6 +1,11 @@
 const answer1 = confirm('JavaScript появился в 1995 году?');
-if(answer1 === true) alert('Ответ верный')
-else alert('Правильный ответ: Да')
+if(answer1 === true) alert('Ответ верный');
+else alert('Ответ неверный. Правильный ответ: Да');
 
 const answer2 = confirm('Спецификация JavaScript называется ECMAScript?');
+if(answer2 === true) alert('Ответ верный');
+else alert('Ответ неверный. Правильный ответ: Да');
+
 const answer3 = confirm('JavaScript был создан за 1 месяц?');
+if(answer3 === false) alert('Ответ верный');
+else alert('Ответ неверный. Правильный ответ: Нет, 10 дней.');
