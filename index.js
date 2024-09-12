2f3589cc477f5e8fcf747d50b99668b94937efe5
pugnitator <pugnitator2000@gmail.com>
2024-09-12 21:51:35 +0300

    M1_17_task2

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
-const getTodosByIds = (ids) => {
-  const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
-  Promise.all(requests)
-    .then((responses) => {
-      const dataResults = responses.map((data) => data.json());
-      return Promise.all(dataResults)
+const TODOS_URL = "https://jsonplaceholder.typicode.com/todos";
+const getTodosByIds = async (ids) => {
+  try {
+    const requests = await ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+    // console.log(requests);
+    const responses = await Promise.all(requests);
+    responses.forEach((response) => {
+        if(!response.ok) throw new Error("response error");
     })
-    .then((allTasks) => {
-      console.log(allTasks);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-}
+    const dataResults = responses.map((data) => data.json());
+    const allTasks = await Promise.all(dataResults);
+    console.log(allTasks);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 getTodosByIds([43, 21, 55, 100, 10]);
