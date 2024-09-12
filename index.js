4a75204733337c107c5d52687233a0d556a94f28
pugnitator <pugnitator2000@gmail.com>
2024-09-12 19:39:42 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,19 @@
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+let isLoading = false;
+const createNewPost = () => {
+  isLoading = true;
+  fetch(POSTS_URL, {
+    method: "POST"
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("result", result);
+    })
+    .catch((error) => {
+      console.log("error", error);
+    })
+    .finally(() => {
+      isLoading = false;
+    });
+};
+createNewPost();
