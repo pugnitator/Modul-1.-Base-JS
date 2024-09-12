9ac53c4ba8d885db8225efe6578d6ce78512761f
pugnitator <pugnitator2000@gmail.com>
2024-09-12 19:53:52 +0300

    M1_17_task1

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 let isLoading = false;
-const createNewPost = () => {
-  isLoading = true;
-  fetch(POSTS_URL, {
-    method: "POST"
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("result", result);
-    })
-    .catch((error) => {
-      console.log("error", error);
-    })
-    .finally(() => {
-      isLoading = false;
-    });
-};
+const createNewPost = async () => {
+    try {
+        isLoading = true;
+        const respose = await fetch(POSTS_URL, {
+          method: "POST"
+        });
+        if(!respose.ok) throw new Error('error', error);
+        const result = await respose.json();
+        console.log(result);
+    } catch(error) {
+        console.log("error", error);
+    } finally {
+        isLoading = false;
+    }
+
+}
+
 createNewPost();
